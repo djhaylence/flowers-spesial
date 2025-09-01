a82f0d9279ff140048e0c8cf40eefa7488701299
djhaylence <raisahmdhh@gmail.com>
2025-09-01 21:07:08 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('ILY, Bubub Icab').split('')
+    const titles = ('HAPPY FAILED MENSIVE').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
